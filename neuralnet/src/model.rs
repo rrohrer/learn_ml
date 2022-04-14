acc2cfe85cf4241d9109c3c3c77a4481039fc4bf
rrohrer <ryan.rohrer@gmail.com>
2022-04-13 23:21:30 -0700

    Added RefCell to Model for nodes

--- neuralnet/src/model.rs
+++ neuralnet/src/model.rs
@@ -1,10 +1,11 @@
 use crate::node::Node;
 use rand::rngs::SmallRng;
 use rand::SeedableRng;
+use std::cell::RefCell;
 
 pub struct Model {
     name: String,
-    nodes: Vec<Box<dyn Node>>,
+    nodes: Vec<RefCell<Box<dyn Node>>>,
     rng: SmallRng,
 }
 
@@ -21,20 +22,20 @@ impl Model {
     /// Adds a node to the graph, and returns its index.
     pub fn add_node(&mut self, node: Box<dyn Node>) -> usize {
         let result = self.nodes.len();
-        self.nodes.push(node);
+        self.nodes.push(RefCell::new(node));
         result
     }
 
     /// create an edge connecting two nodes
     pub fn create_edge(&mut self, src: usize, dst: usize) {
-        self.nodes[src].add_subsequent_node(dst);
-        self.nodes[dst].add_antecent_node(src);
+        self.nodes[src].borrow_mut().add_subsequent_node(dst);
+        self.nodes[dst].borrow_mut().add_antecent_node(src);
     }
 
     /// initialize all of the nodes in the model, using the given seed
     pub fn init(&mut self) {
         for node in &mut self.nodes {
-            node.init(self.rng.clone());
+            node.borrow_mut().init(self.rng.clone());
         }
     }
 
